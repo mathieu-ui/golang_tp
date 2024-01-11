b7a5fbc26b04a645b4c5c7b5a3ae612e5313fd8a
mathieu-ui <puig.mathieu.pro@gmail.com>
2024-01-11 11:02:27 +0100

    Adding feat : add sheep and print all sheep

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -12,6 +13,8 @@ import (
 func main() {
 	fmt.Println("111")
 
+	f := make(Ferme)
+
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
@@ -19,27 +22,37 @@ func main() {
 		w.Write([]byte("Hello World!"))
 	})
 
+	r.Get("/moutonlist", func(w http.ResponseWriter, r *http.Request) {
+		for sheep := range f {
+			a := "Nom: " + f[sheep].Name + ", age: " + strconv.Itoa(f[sheep].Age) + ", poid: " + strconv.FormatFloat(f[sheep].Weight, 'f', 2, 64) + "\n"
+			fmt.Println(a)
+			w.Write([]byte(a))
+		}
+	})
+
 	r.Post("/mouton", func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
-		// var m Sheep
+		var m Sheep
 
-		//err := json.NewDecoder(r.Body).Decode(&m)
+		err := json.NewDecoder(r.Body).Decode(&m)
 
-		//fmt.Println()
-		// if err != nil {
-		// 	fmt.Println(err)
-		// 	http.Error(w, err.Error(), http.StatusBadRequest)
-		// 	return
-		// }
-		// fmt.Fprintf(w, "Person: %+v", m)
-
-		b, err := io.ReadAll(r.Body)
-		if err == nil {
-			fmt.Println(b)
-			myString := string(b[:])
-			fmt.Println(myString)
-			fmt.Println(myString[12])
+		fmt.Println()
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
 		}
+		fmt.Println(m)
+
+		AddSheep(f, m.Id, m.Id, m.Name, m.Age, m.Weight)
+
+		// b, err := io.ReadAll(r.Body)
+		// if err == nil {
+		// 	fmt.Println(b)
+		// 	myString := string(b[:])
+		// 	fmt.Println(myString)
+		// 	fmt.Println(myString[12])
+		// }
 
 	})
 
@@ -47,9 +60,24 @@ func main() {
 
 }
 
+type Ferme map[int]Sheep
+
 type Sheep struct {
 	Id     int
 	Name   string
 	Age    int
 	Weight float64
+}
+
+func NewSheep(idSheep int, nameSheep string, ageSheep int, weightSheep float64) Sheep {
+	nSheep := Sheep{
+		Id:     idSheep,
+		Name:   nameSheep,
+		Age:    ageSheep,
+		Weight: weightSheep}
+	return nSheep
+}
+
+func AddSheep(f1 Ferme, cle int, id int, nom string, age int, poid float64) {
+	f1[cle] = NewSheep(id, nom, age, poid)
 }
